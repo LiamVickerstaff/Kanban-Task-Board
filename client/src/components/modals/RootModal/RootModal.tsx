11741553bb6f9c1e56cbd282e655027eb2e41a2b
Liam V <liamvickerstaff050@gmail.com>
2026-02-04 12:33:41 +1100

    Modal fade in animation

--- client/src/components/modals/RootModal/RootModal.tsx
+++ client/src/components/modals/RootModal/RootModal.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import styles from "./RootModal.module.css";
 import { createPortal } from "react-dom";
 import useModalStore from "../../../stores/useModalStore";
+import { easeIn, easeInOut, easeOut, motion } from "motion/react";
 
 export default function RootModal() {
   const { isOpen, content, close } = useModalStore();
@@ -20,12 +21,15 @@ export default function RootModal() {
 
   return createPortal(
     <div className={styles.pageOverlay} onClick={close}>
-      <div
+      <motion.div
+        initial={{ opacity: 0, y: "-2rem" }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: easeOut }}
         className={styles.modalContainer}
         onClick={(e) => e.stopPropagation()}
       >
         <div className={styles.modalScrollArea}>{content}</div>
-      </div>
+      </motion.div>
     </div>,
     document.body,
   );
